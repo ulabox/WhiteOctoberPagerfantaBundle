4ebad2dbc31c0115cd73f49585b81fb301953e70
Albert Casademont <albert@ulabox.com>
2011-08-17 16:26:13 +0200

    Revert "Added support for column order"
    This reverts commit dba5d70e2f998e41a9ceaddbecc1764fc689b2d3.

--- Twig/PagerfantaExtension.php
+++ Twig/PagerfantaExtension.php
@@ -40,7 +40,6 @@ class PagerfantaExtension extends \Twig_Extension
     {
         return array(
             'pagerfanta'  => new \Twig_Function_Method($this, 'renderPagerfanta', array('is_safe' => array('html'))),
-            'sortcol'  => new \Twig_Function_Method($this, 'setSortColumns', array('is_safe' => array('html'))),
         );
     }
 
@@ -81,50 +80,6 @@ class PagerfantaExtension extends \Twig_Extension
         };
 
         return $this->container->get('white_october_pagerfanta.view_factory')->get($viewName)->render($pagerfanta, $routeGenerator, $options);
-    }
-    
-    /**
-     * Renders a pagerfanta.
-     *
-     * @param string              $sortCol    The column order name
-     * @param array               $options    An array of options (optional).
-     *
-     * @return string The pagerfanta rendered.
-     */
-    public function setSortColumns($sortCol, array $options = array())
-    {
-        $options = array_replace(array(
-            'routeName'   => null,
-            'routeParams' => array('order' => 'id', 'dir' => 'asc'),
-            'target' => null,
-            'name' => ucwords(str_replace('_',' ', $sortCol)),
-        ), $options);
-
-        $router = $this->container->get('router');
-        $request = $this->container->get('request');
-
-        if (null === $options['routeName']) {
-            $options['routeName'] = $request->attributes->get('_route');
-            $options['routeParams'] = array_replace($options['routeParams'], $request->query->all());
-            if ($options['routeName'] === '_internal') {
-                throw new \Exception('PagerfantaBundle can not guess the route when used in a subrequest');
-            }
-            foreach ($router->getRouteCollection()->get($options['routeName'])->compile()->getVariables() as $variable) {
-                $options['routeParams'][$variable] = $request->attributes->get($variable);
-            }
-        }
-
-        
-        $routeName = $options['routeName'];
-        $routeParams = $options['routeParams'];
-        $is_active = ($routeParams['order'] === $sortCol);
-        $routeParams['dir'] = ($is_active && $routeParams['dir'] === 'asc') ? 'desc' : 'asc';
-        $route = $router->generate($routeName, array_merge($routeParams, array('order' => $sortCol)));
-
-        $triangle = ($is_active) ? ' <span class="triangle">'. (($routeParams['dir'] === 'asc') ? '&#9660;' : '&#9650;') .'</span>' : '';
-        $target = (!empty($options['target'])) ? ' target="'. $options['target'] .'"' : '';
-        $class = ($is_active) ? ' class="active"' : '';
-        return '<a href="'. $route .'"'. $target . $class .'>'. $options['name'] . $triangle .'</a>';
     }
 
     /**
